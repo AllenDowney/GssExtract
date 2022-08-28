dd6bfccae39ac8d02b35d718d90a5fdac6946353
Allen Downey <downey@allendowney.com>
2022-08-28 09:29:38 -0400

    Adding cookiecutter elements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='gss-extract',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for making extracts of GSS data',
+    author='Allen Downey',
+    license='MIT',
+)
